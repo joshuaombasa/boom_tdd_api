568f2654af4785ac0e4f8db719a85a548c8fb74a
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-03 22:43:45 +0300

    vi-boom-tdd

---
+++ app.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const config  =require('./utils/config')
+
+const warehousesRouter = require('./controllers/warehouses')
+
+const app = express()
+
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+        .then(() => logger.info('connected to MongoDB'))
+        .catch(error => logger.error(error.message))
+
+
+app.use(express.json())
+app.use(cors())
+app.use(middleware.requestLogger)
+
+app.use('/api/warehouses', warehousesRouter)
+
+app.use(middleware.unknownEndpointHandler)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
