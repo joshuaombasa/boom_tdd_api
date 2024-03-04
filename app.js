e0d5798cafe3e7b585f807667007acb2bf23b945
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-04 23:04:12 +0300

    v2_boom_tdd_api

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-const config  =require('./utils/config')
+const config = require('./utils/config')
 
 const warehousesRouter = require('./controllers/warehouses')
+const loginRouter = require('./controllers/login')
+const ownersRouter = require('./controllers/owners')
 
 const app = express()
 
@@ -22,6 +24,8 @@ app.use(cors())
 app.use(middleware.requestLogger)
 
 app.use('/api/warehouses', warehousesRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/owners', ownersRouter)
 
 app.use(middleware.unknownEndpointHandler)
 app.use(middleware.errorHandler)
